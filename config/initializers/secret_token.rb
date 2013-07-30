265fbc7dd8db50cc4584dd36bb6250f5cc6cb52a
Isabel Cortes <belcortes@gmail.com>
2013-07-30 09:37:37 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Calcacademy::Application.config.secret_key_base = 'e63fe5643b8ee915cbb699924d5ce3c345af770aec81cba0ec822f2af98209598952239fffdbf0d31b55feb6e1a40d6a34e58bba232527b9d0e1e7c546d12878'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Calcacademy::Application.config.session_store :cookie_store, key: '_calcacademy_session'
