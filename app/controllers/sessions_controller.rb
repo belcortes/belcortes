46370a141f065c1603698de9cd8c93d58453f742
Isabel Cortes <belcortes@gmail.com>
2013-08-06 11:40:09 -0400

    trying to connect with fb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,9 @@ class SessionsController < ApplicationController
   def new
   end
  def create
+    if FB.user_id == true && !@user#they logged in with facebook && they dont have an account with calcacademy
+      @user = User.new
+    end
     user = User.find_by_email(params[:email])
     authenticated_user = user.authenticate(params[:password]) if user
     p 'almost authenticated'
